7109c617c38569f90ac5e4cbafc2d511c010c8f1
Sparsh Bansal <sparshbansal787@gmail.com>
2025-06-13 09:28:17 +0530

    [Sparsh] Refactor . Improved Method Naming

---
+++ ControlFlows/Level1/Divisibility5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Divisibility5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+
+        if (number % 5 == 0) {
+            System.out.println("Is the number " + number + " divisible by 5? Yes");
+        } else {
+            System.out.println("Is the number " + number + " divisible by 5? No");
+        }
+
+        scanner.close();
+    }
+}
