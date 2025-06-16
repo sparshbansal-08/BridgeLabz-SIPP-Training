59a6eb5785c8e2322fbcd35d7f81a153d8029429
Sparsh Bansal <sparshbansal787@gmail.com>
2025-06-16 09:12:27 +0530

    [Sparsh] Add . Level 1 completed

---
+++ Array/Level1/Copy2DTo1D.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Copy2DTo1D {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+        int[] array = new int[rows * cols];
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Enter value for element [" + i + "][" + j + "]: ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[index++] = matrix[i][j];
+            }
+        }
+
+        System.out.print("1D Array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ Array/Level1/Multiplicationsixtonine.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Multiplicationsixtonine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int[] multiplicationResult = new int[4];
+
+        for (int i = 6; i <= 9; i++) {
+            multiplicationResult[i - 6] = number * i;
+        }
+
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + multiplicationResult[i - 6]);
+        }
+    }
+}
